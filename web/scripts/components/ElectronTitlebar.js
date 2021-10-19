049ec3cd6ca69edadc5218c75bea60ac980c69e5
Uwe <uriegel@hotmail.de>
2021-10-19 07:43:49 +0200

    Titlebar, menu and virtual table

---
+++ web/scripts/components/ElectronTitlebar.js
@@ -0,0 +1,97 @@
+const electron = window.require('electron')
+
+class ElectronTitlebar extends HTMLElement {
+    constructor() {
+        super()
+        
+        var style = document.createElement("style")
+        document.head.appendChild(style)
+        style.sheet.insertRule(`:root {
+            --electron-titlebar-color: black;
+            --electron-titlebar-background-color: #eee;
+            --electron-titlebar-button-hover-color: lightgray;
+            --electron-titlebar-height: 30px;
+        }`)
+        
+        this.attachShadow({ mode: 'open' })
+
+        const template = document.createElement('template')
+        template.innerHTML = ` 
+            <style>
+                .titlebar {
+                    display: flex;
+                    color: var(--electron-titlebar-color);
+                    background-color: var(--electron-titlebar-background-color);
+                    height: var(--electron-titlebar-height);
+                }
+                #icon {
+                    height: var(--electron-titlebar-height);
+                }
+                .dragregion {
+                    flex-grow: 1;
+                    text-align: center;
+                    vertical-align: middle;
+                    margin: 3px 3px 0px 0px;
+                    -webkit-app-region: drag;
+                    display: flex;
+                    align-items: center;   
+                }
+                #title {
+                    flex-grow: 1;   
+                    margin-top: -3px;        
+                }
+                .button {
+                    width: 44px;
+                    text-align: center;
+                    font-size: 12pt;
+                    display: flex;
+                    align-items: center;        
+                    cursor: pointer;
+                }
+                .button>span {
+                    flex-grow: 1;   
+                    margin-top: -3px;
+                    user-select: none;
+                }
+                .button:hover {
+                    background-color: var(--electron-titlebar-button-hover-color);
+                }
+                .close:hover {
+                    background-color: red;
+                    color: white;
+                }
+                .button>span.dash {
+                    vertical-align: sub;
+                    margin-top: 0px;
+                }                             
+            </style>
+            <div class="titlebar">
+                <img id="icon">
+                <slot></slot>
+                <div class="dragregion">
+                    <span id="title"></span>
+                </div>
+                <div id="minimize" class="button" @click="onMinimize"><span class="dash">&#x2012;</span></div>
+                <div id="maximize" class="button" @click="onMaximize"><span>&#9744;</span></div>                
+                <div id="close" class="button close"><span>&#10005;</span></div>
+            </div>
+        `
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+        const icon = this.shadowRoot.getElementById("icon")
+        icon.src = this.getAttribute("icon")
+        const title = this.shadowRoot.getElementById("title")
+        title.innerText = this.getAttribute("title")
+        this.minimize = this.shadowRoot.getElementById("minimize")
+        this.maximize = this.shadowRoot.getElementById("maximize")
+        this.close = this.shadowRoot.getElementById("close")
+    }
+
+    connectedCallback() {
+        this.minimize.addEventListener("click", () => electron.ipcRenderer.send("minimize"))
+        this.maximize.addEventListener("click", () => electron.ipcRenderer.send("maximize"))
+        this.close.addEventListener("click", () => close())
+    }
+}
+
+customElements.define('electron-titlebar', ElectronTitlebar)
---
+++ web/scripts/menu.js
@@ -0,0 +1,35 @@
+//import { setHiddenCallback } from "./index.js"
+const electron = window.require('electron')
+
+window.onRename = () => {
+    alert("Rename")
+}
+
+window.onExtendedRename = () => {
+    alert("Extended Rename")
+}
+
+window.onCopy = () => {
+    alert("Copy Files")
+}
+
+window.onMove = () => {
+    console.log("Move Files")
+}
+
+window.onDarkTheme = isChecked => {
+    if (isChecked)
+        document.body.classList.add("darkTheme")
+    else
+        document.body.classList.remove("darkTheme")
+}
+
+window.onClose = () => close()
+
+window.onHidden = isChecked => {
+    console.log(`Show hidden ${isChecked}`)
+}
+
+window.setHidden = mi => {} //setHiddenCallback(isChecked => mi.isChecked = isChecked)
+
+window.onDevTools = () => electron.ipcRenderer.send("openDevTools")
