b1a57b1f139e110501461bd9d0cf88069c80f0eb
Uwe Riegel <uriegel@hotmail.de>
2022-01-09 18:09:17 +0100

    GetIcon (Windows) Rust

--- electron/platform/windows.js
+++ electron/platform/windows.js
@@ -1,26 +1,17 @@
 const { protocol } = require("electron")
-const { getIcon74 } = require("../../index.node")
+const { getIcon } = require("../../index.node")
 
-
-const { getIcon } = require("filesystem-utilities")
-
+const getIconAsnyc = ext => new Promise(res => getIcon(ext, 16, (err, buffer) => res(buffer)))
 
 function registerGetIconProtocol() {
     protocol.registerBufferProtocol('icon', async (request, callback) => {
         const url = request.url
         var ext = url.substring(7)
-        var icon = await getIcon(ext)
+        var icon = await getIconAsnyc(ext)
         callback({ mimeType: 'img/png', data: icon })
     }, (error) => {
         if (error) console.error('Failed to register protocol', error)
     })
-
-    // protocol.registerFileProtocol('icon', async (request, callback) => {
-    //     const url = request.url
-    //     var ext = url.substring(7)
-    //     var path = getIcon(ext, 16)
-    //     callback(path)
-    // })
 }
 
 exports.registerGetIconProtocol = registerGetIconProtocol
--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -1,34 +1,46 @@
 use systemicons::get_icon;
 use neon::prelude::*;
 
+// static THREAD_POOL: Lazy<ThreadPool> = Lazy::new(|| {
+//     ThreadPool::new(4)
+// });
+
 fn get_icon_async(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+
     let ext = cx.argument::<JsString>(0)?.value(&mut cx);
     let size = cx.argument::<JsNumber>(1)?.value(&mut cx);
     let callback = cx.argument::<JsFunction>(2)?.root(&mut cx);
-
-    let args = match get_icon(&ext, size as i32) {
-        Ok(buffer) => {
-            let mut js_buffer = cx.buffer(buffer.len() as u32)?;
-            cx.borrow_mut(&mut js_buffer, |js_buffer| {
-                let buf = js_buffer.as_mut_slice();
-                buf.copy_from_slice(&buffer);
-            });
-        
-            vec![
-                cx.null().upcast::<JsValue>(),
-                js_buffer.upcast(),
-            ]
-        }
-        Err(_err) => {
-            let err = cx.string("Could not get icon buffer");
-            vec![
-                err.upcast::<JsValue>(),
-            ]            
-        }
-    };
-    let this = cx.undefined();
-    let callback = callback.into_inner(&mut cx);
-    callback.call(&mut cx, this, args)?;
+    let channel = cx.channel();
+    
+    std::thread::spawn(move || {
+        let result = get_icon(&ext, size as i32);
+        channel.send(move |mut cx| {
+            let args = match result {
+                Ok(buffer) => {
+                    let mut js_buffer = cx.buffer(buffer.len() as u32)?;
+                    cx.borrow_mut(&mut js_buffer, |js_buffer| {
+                        let buf = js_buffer.as_mut_slice();
+                        buf.copy_from_slice(&buffer);
+                    });
+                
+                    vec![
+                        cx.null().upcast::<JsValue>(),
+                        js_buffer.upcast(),
+                    ]
+                }
+                Err(_err) => {
+                    let err = cx.string("Could not get icon buffer");
+                    vec![
+                        err.upcast::<JsValue>(),
+                    ]            
+                }
+            };
+            let this = cx.undefined();
+            let callback = callback.into_inner(&mut cx);
+            callback.call(&mut cx, this, args)?;
+            Ok(())
+        });
+    });
     Ok(cx.undefined())
 }
 
