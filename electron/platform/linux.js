51ba397b49bedfcfdf900bb1b1e758e8df3266d8
uriegel <uriegel@hotmail.de>
2022-01-09 23:55:04 +0100

    getFiles Linux Rust

--- electron/platform/linux.js
+++ electron/platform/linux.js
@@ -10,4 +10,6 @@ function registerGetIconProtocol() {
     })
 }
 
+
+
 exports.registerGetIconProtocol = registerGetIconProtocol
--- rust-test/indexLinux.js
+++ rust-test/indexLinux.js
@@ -1,9 +1,13 @@
-const { initGtk, getIcon } = require("../index.node")
+const { initGtk, getIcon, getFiles } = require("../index.node")
 
 console.log("Testing Rust (Javascript side)")
 
 initGtk()
 
 console.log("system icon", getIcon(".pdf", 16))
+
+var files = getFiles("/home/uwe")
+console.log("files")
+console.log("files", files)
 
 
