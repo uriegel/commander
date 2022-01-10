efd4653eff62f567fef0cda342c13f66b2483345
uriegel <uriegel@hotmail.de>
2022-01-10 13:27:27 +0100

    copyFiles asynchron and copy status polling (Linux)

--- rust-test/indexLinux.js
+++ rust-test/indexLinux.js
@@ -1,4 +1,4 @@
-const { initGtk, getIcon, getFiles, copyFile } = require("../index.node")
+const { initGtk, getIcon, getFiles, copyFile, getCopyStatus } = require("../index.node")
 
 console.log("Testing Rust (Javascript side)")
 
@@ -6,8 +6,22 @@ initGtk()
 
 console.log("system icon", getIcon(".pdf", 16))
 
-var files = getFiles("/home/uwe")
+
+
+//var timer = setTimeout(() => console.log("Progress", getCopyStatus()), 40)
+//var files = getFiles("/home/uwe")
+var files = getFiles(("/home/uwe"))
+//clearTimeout(timer)
 console.log("files")
 console.log("files", files)
 
-copyFile()
+const copyFileAsnyc = () => new Promise(res => copyFile(res))
+
+const run = async () => {
+    var timer = setInterval(() => console.log("Progress", getCopyStatus()), 100)
+    await copyFileAsnyc()
+    clearInterval(timer)
+}
+
+run()
+
--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -4,6 +4,30 @@ use systemicons::init;
 use systemicons::get_icon_as_file;
 use neon::prelude::*;
 
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+trait Tr {
+    fn get_counter() -> &'static AtomicUsize {
+        static COUNTER: AtomicUsize = AtomicUsize::new(0);
+        &COUNTER
+    }
+}
+
+struct CopyStatus { }
+
+impl<CopyStatus> Tr for CopyStatus { }
+
+impl CopyStatus {
+    fn get_value() -> usize {
+        let counter = CopyStatus::get_counter();
+        counter.fetch_add(0, Ordering::Relaxed)
+    }
+    fn add_value(val: usize) {
+        let counter = CopyStatus::get_counter();
+        counter.fetch_add(val, Ordering::Relaxed);
+    }
+}
+
 pub fn is_hidden(name: &str, _: &Metadata)->bool {
     name.as_bytes()[0] == b'.' && name.as_bytes()[1] != b'.'
 }
@@ -12,6 +36,7 @@ pub fn init_addon(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("initGtk", init_gtk)?;
     cx.export_function("getIcon", get_icon)?;
     cx.export_function("copyFile", copy_file)?;
+    cx.export_function("getCopyStatus", get_copy_status)?;
     Ok(())
 }
 
@@ -32,11 +57,30 @@ fn copy_file(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     let file = File::for_path("/home/uwe/Videos/raw/Goldeneye.mts");
     let target = File::for_path("/home/uwe/test/affe.mts");
 
-    let mut affe = |a, b |{
-        println!("Progess: {}, {}", a, b);
-    };
+    let callback = cx.argument::<JsFunction>(0)?.root(&mut cx);
+    let channel = cx.channel();
+    
+    std::thread::spawn(move || {
+        let mut affe = |a, b |{
+            CopyStatus::add_value(a as usize);
+            // let affe = CopyStatus::get_value();
+            // println!("Progess: {}, {} {}", a, b, affe);
+        };
 
-    let res = file.copy(&target, FileCopyFlags::OVERWRITE | FileCopyFlags::ALL_METADATA, Some(&gio::Cancellable::new()), 
-        Some(&mut affe));
+        let res = file.copy(&target, FileCopyFlags::OVERWRITE | FileCopyFlags::ALL_METADATA, Some(&gio::Cancellable::new()), 
+            Some(&mut affe));
+        channel.send(move |mut cx| {
+            let this = cx.undefined();
+            let callback = callback.into_inner(&mut cx);
+            let args = vec![ cx.null().upcast::<JsValue>() ];
+            callback.call(&mut cx, this, args)?;
+            Ok(())
+        });
+    });
     Ok(cx.undefined())
+}
+
+fn get_copy_status(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    let affe = CopyStatus::get_value();
+    Ok(cx.number(affe as f64))
 }
