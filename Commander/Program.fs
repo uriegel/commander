28ce2028603344c6c7f39ee363bc6bc645ae39bf
Uwe Riegel <uriegel@hotmail.de>
2022-03-02 16:43:26 +0100

    README

--- Commander/Program.fs
+++ Commander/Program.fs
@@ -1,1 +1,13 @@
 ﻿printfn "Hello Commander"
+
+TODO Electron global installieren (README)
+TODO Start electron http://google.de
+TODO on electron exit end program
+TODO typescript electron main
+TODO Dotnet F# exe (Windows winexe) Giraffe
+TODO index and css and styles in Resource, served by Giraffe
+TODO Start electron http://localhost:9865/commander?x=23&y=45&w=234&h=234&isMaximized, wait for electron exit
+TODO Windows and Linux
+TODO typescript renderer script
+
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # window-commander
 A Norton Commander clone based on Electron (globally insdtalled), F#, Typescript and Web Components
 
+## Setup
+`npm i -g electron`
