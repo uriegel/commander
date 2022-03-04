4221ea6e3a01f9e4a73ef1908220884c3586e537
Uwe Riegel <uriegel@hotmail.de>
2022-03-04 21:09:07 +0100

    send windows bounds and icon

--- Commander/Program.fs
+++ Commander/Program.fs
@@ -6,7 +6,7 @@ async {
     do! Electron.start <| saveResource (getElectronFile "main.js", "electron/main.js")
 } |> Async.RunSynchronously
 
-//TODO Request to set window settings
+//TODO Request to save window settings
 //TODO Start electron with environment x=23 y=45 w=234 h=234 isMaximized
 //TODO typescript renderer script
 
