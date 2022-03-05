bb741773560ebea94b8c2cb70cde3110ae7b4e1b
Uwe Riegel <uriegel@hotmail.de>
2022-03-05 23:35:08 +0100

    TODO

--- Commander/Program.fs
+++ Commander/Program.fs
@@ -6,4 +6,13 @@ async {
     do! Electron.start <| saveResource (getElectronFile "main.js", "electron/main.js")
 } |> Async.RunSynchronously
 
-// TODO automatic theme controlling Linux Windows
+// TODO automatic theme controlling Linux 
+// [uwe@illmatic ~]$ gsettings monitor org.gnome.desktop.interface gtk-theme
+//  gtk-theme: 'Adwaita-dark'
+//  gtk-theme: 'Adwaita'
+// ^C
+// [uwe@illmatic ~]$ gsettings get org.gnome.desktop.interface gtk-theme
+// 'Adwaita'
+// [uwe@illmatic ~]$ 
+
+// TODO automatic theme controlling Windows 
