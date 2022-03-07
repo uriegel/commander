14d960892a7bab30fde6ece6a164e30b3546d9d0
Uwe <uriegel@hotmail.de>
2022-03-07 13:21:27 +0100

    Automatic theme detection Windows

--- Commander/Program.fs
+++ Commander/Program.fs
@@ -8,6 +8,3 @@ startThemeDetection ()
 async {
     do! Electron.start <| saveResource (getElectronFile "main.js", "electron/main.js")
 } |> Async.RunSynchronously
-
-// TODO automatic theme controlling Windows 
-// https://docs.microsoft.com/en-us/previous-versions/windows/desktop/regprov/registryvaluechangeevent
