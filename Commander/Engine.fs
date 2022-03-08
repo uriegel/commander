b248e334066c217bd9c1111a7124815463960b14
Uwe Riegel <uriegel@hotmail.de>
2022-03-08 16:54:36 +0100

    get root items

--- Commander/Engine.fs
+++ Commander/Engine.fs
@@ -6,6 +6,20 @@ type GetItems = {
     EngineId: int
 }
 
+// TODO Linux Windows
+type RootItem = {
+    Description: string
+    Name:        string
+    Type:        int
+    MountPoint:  string
+    DriveType:   string
+    IsMounted:   bool
+    Size:        int64
+}
+
+
+type GetItemResult = RootItem[]
+
 type IEngine = 
     abstract member Id : int with get
-    abstract member getItems: getItems: GetItems -> Async<string>
+    abstract member getItems: getItems: GetItems -> Async<GetItemResult>
--- Commander/LinuxRoot.fs
+++ Commander/LinuxRoot.fs
@@ -1,15 +1,75 @@
 module Root
 
+open FSharpTools
+
 open Engine
-open Giraffe
 open Utils
 
 type Root () = 
+    let getHomeDir = 
+        let getHomeDir () = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal)
+        memoizeSingle getHomeDir
+
     let getItems (getItems: GetItems) = async {
-        let t = runCmd "lsblk" "--bytes --output SIZE,NAME,LABEL,MOUNTPOINT,FSTYPE"
-        return! t ()
+        let! res = runCmd "lsblk" "--bytes --output SIZE,NAME,LABEL,MOUNTPOINT,FSTYPE" ()
+        let driveStrs = res |> String.splitChar '\n'
+        let columnPositions = 
+            let title = driveStrs[0]
+            let getPart key = title |> String.indexOf key |> Option.defaultValue 0
+            [|
+                0
+                getPart "NAME"
+                getPart "LABEL"
+                getPart "MOUNT"
+                getPart "FSTYPE"
+            |]
+        let constructDrives driveString =
+            let getString pos1 pos2 =
+                driveString 
+                |> String.substring2 columnPositions[pos1] (columnPositions[pos2]-columnPositions[pos1]) 
+                |> String.trim
+            let trimName name = 
+                if name |> String.length > 2 && name[1] = '─' then
+                    name |> String.substring 2
+                else
+                    name
+            let mountPoint = getString 3 4
+
+            {
+                Description = getString 2 3
+                Name        = getString 1 2 |> trimName
+                Type        = 1 // TODO Drivetypes enum DriveType       
+                MountPoint  = mountPoint
+                IsMounted   = mountPoint |> String.length > 0 
+                DriveType   = driveString |> String.substring columnPositions[4] |> String.trim
+                Size        = 999 // TODO size
+            }
+
+        let filterDrives (n: string) = n[columnPositions[1]] > '~'
+        let getItems () = 
+                driveStrs
+                |> Array.skip 1
+                |> Array.filter filterDrives
+                |> Array.map constructDrives
+
+        let items = getItems ()
+        let mounted = items |> Array.filter (fun n -> n.IsMounted)
+        let unMounted = items |> Array.filter (fun n -> not n.IsMounted)
+        return Array.concat [ 
+            [| { 
+                Description = "home"
+                Name = "~"
+                MountPoint = getHomeDir ()
+                IsMounted = true
+                Type = 1
+                Size = 0
+                DriveType = "" 
+            } |]
+            mounted
+            unMounted
+        ]
     }
     interface IEngine with
         member val Id = 1 with get
-        member __.getItems (param: GetItems) = getItems param
+        member _.getItems (param: GetItems) = getItems param
 
