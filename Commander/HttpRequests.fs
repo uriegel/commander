5d85ac3550988ebed9e517546de0bea23a0c7d3e
Uwe Riegel <uriegel@hotmail.de>
2022-03-25 13:17:17 +0100

    Get items from android

---
+++ Commander/HttpRequests.fs
@@ -0,0 +1,16 @@
+module HttpRequests
+
+open System
+open System.Net.Http
+open System.Net.Http.Json
+open Configuration
+open System.Threading
+
+let post<'a> url data = task {
+    let httpClient = new HttpClient()
+    use requestMessage = new HttpRequestMessage(HttpMethod.Post, Uri(url))
+    requestMessage.Content <- JsonContent.Create(data, data.GetType (), null, getJsonOptions ()) 
+    use! responseMessage = httpClient.SendAsync(requestMessage) 
+    let! test = responseMessage.Content.ReadAsStringAsync() 
+    return! responseMessage.Content.ReadFromJsonAsync<'a>(getJsonOptions (), CancellationToken.None) 
+} 
