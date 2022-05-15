ddc187ade58b19ad871e730872537c8b24a18cba
Uwe Riegel <uriegel@hotmail.de>
2022-05-15 10:41:24 +0200

    TODO

--- scripts/components/folder.ts
+++ scripts/components/folder.ts
@@ -306,6 +306,7 @@ export class Folder extends HTMLElement {
 
         this.onPathChanged(result.path, fromBacklog)
 
+        // TODO Windows: save bounds
         // TODO Maximize: toggle Restore
         // TODO Fullscreen: hide electron title bar
         // TODO rename Windows
