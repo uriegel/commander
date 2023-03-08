feb6984a12adfc1fbdbda2fa8ad71f3199bde64e
Uwe Riegel <uriegel@hotmail.de>
2023-03-08 08:44:30 +0100

    TODOs moved

--- Commander/Program.cs
+++ Commander/Program.cs
@@ -7,10 +7,3 @@
     .Build()
     .Run("de.uriegel.Commander");
 
-// TODO run javascript get button, set click handler, show devtools
-// TODO save/restore bounds if requested
-// TODO host web site in kestrel and resources
-// TODO To GtkDotNet nuget 0.0.1alpha
-// TODO Then to GtkDotNet.Linux nuget 0.0.1alpha
-// TODO Windows version
-
