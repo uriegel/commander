1fd960cb0a795a41f426fcf25873406320467263
Uwe Riegel <uriegel@hotmail.de>
2023-03-19 09:38:08 +0100

    // TODO

--- Commander/platforms/Linux/Directory.cs
+++ Commander/platforms/Linux/Directory.cs
@@ -25,3 +25,39 @@ static partial class Directory
 }
 
 #endif
+
+    // TODO KDE
+    // let getKdeIcon ext = async {
+    //     let extractMime str = 
+    //         let pos1 = str |> String.indexOf "('" 
+    //         let pos2 = str |> String.indexOf "',"
+    //         match pos1, pos2 with
+    //         | Some pos1, Some pos2 
+    //             -> Some (str |> String.substring2 (pos1+2) (pos2-pos1-2))
+    //         | _ -> None
+
+    //     let replaceSlash str = Some (str |> String.replaceChar  '/' '-')
+    //     let getMime = extractMime >=> replaceSlash
+
+    //     let mapVarious mime =
+    //         match mime with
+    //         | "/usr/share/icons/breeze/mimetypes/16/application-x-msdos-program.svg" 
+    //                         -> "/usr/share/icons/breeze/mimetypes/16/application-x-ms-dos-executable.svg"
+    //         | "/usr/share/icons/breeze/mimetypes/16/application-java-archive.svg"    
+    //                         -> "/usr/share/icons/breeze/mimetypes/16/application-x-jar.svg"
+    //         | s     -> s
+
+    //     let! mimeType = asyncRunCmd "python3" (sprintf "%s *%s" (getIconScript ()) ext)
+
+    //     let icon = 
+    //         sprintf "/usr/share/icons/breeze/mimetypes/16/%s.svg" (mimeType |> getMime |> defaultValue "application-x-zerosize")
+    //         |> mapVarious
+    //         |> getExistingFile
+    //         |> Option.defaultValue "/usr/share/icons/breeze/mimetypes/16/application-x-zerosize.svg"
+    //     return icon, "image/svg+xml"
+    // }
+
+//     return! 
+//         match getPlatform () with
+// //        | Platform.Kde -> getKdeIcon ext
+//         | _            -> async { return getIcon ext, "image/png" }
--- Commander/platforms/Windows/Directory.cs
+++ Commander/platforms/Windows/Directory.cs
@@ -7,6 +7,48 @@ static partial class Directory
         ? info.FullName
         : info.Extension?.Length > 0 ? info.Extension 
         : ".noextension";
+
+    public static Task ProcessIcon(HttpContext context, string iconHint)
+        => RepeatOnException(() => 
+            WebWindowNetCore.WebView.GtkApplication!.Dispatch(async () =>
+                {
+                    using var iconInfo = IconInfo.Choose(iconHint, 16, IconLookup.ForceSvg);
+                    var iconFile = iconInfo.GetFileName();
+                    using var stream = iconFile?.OpenFile();
+                    await context.SendStream(stream!, null);
+                }, 100), 
+            1);
+open ClrWinApi
+    // let rec getIconHandle callCount = async {
+    //     if ext |> String.contains "\\" then
+    //         return Icon.ExtractAssociatedIcon(ext).Handle
+    //     else
+    //         let mutable shinfo = ShFileInfo()
+    //         SHGetFileInfo(ext, FileAttributeNormal, &shinfo, Marshal.SizeOf shinfo,
+    //             SHGetFileInfoConstants.ICON
+    //             ||| SHGetFileInfoConstants.SMALLICON
+    //             ||| SHGetFileInfoConstants.USEFILEATTRIBUTES
+    //             ||| SHGetFileInfoConstants.TYPENAME) |> ignore
+
+    //         if shinfo.IconHandle <> IntPtr.Zero then
+    //             return shinfo.IconHandle
+    //         elif callCount < 3 then
+    //             do! Async.Sleep 29
+    //             return! getIconHandle <| callCount + 1
+    //         else
+    //             return Icon.ExtractAssociatedIcon(@"C:\Windows\system32\SHELL32.dll").Handle
+    // }
+
+    // let! iconHandle = getIconHandle 0
+    // use icon = Icon.FromHandle iconHandle
+    // use bitmap = icon.ToBitmap()
+    // let ms = new MemoryStream()
+    // bitmap.Save(ms, ImageFormat.Png)
+    // ms.Position <- 0L
+    // DestroyIcon iconHandle |> ignore
+    // return ms
+
+
 }
 
 #endif
