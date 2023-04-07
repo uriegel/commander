c04e013b29c1c24f32fd5f70d777d5ea99a6be94
Uwe Riegel <uriegel@hotmail.de>
2023-04-07 13:41:32 +0200

    CopyItemsFromRemote

--- Commander/Remote.cs
+++ Commander/Remote.cs
@@ -20,11 +20,11 @@ static partial class Remote
         };
 
     public static Task<GetFilesResult> GetFiles(GetFiles getFiles)
-        => (from n in Request.GetStringAsync(GetFiles(getFiles.Path.StringBetween('/', '/'),
-                () => JsonContent.Create(new
-                {
-                    Path = "/" + getFiles.Path.SubstringAfter('/').SubstringAfter('/')
-                })))
+        => (from n in Request.GetStringAsync(
+                getFiles
+                    .Path
+                    .GetIpAndPath()
+                    .GetFiles())
             select JsonSerializer
                     .Deserialize<RemoteItem[]>(n, JsonWebDefaults)
                     .GetOrDefault(Array.Empty<RemoteItem>())
@@ -34,13 +34,22 @@ static partial class Remote
                     .GetOrDefaultAsync(new GetFilesResult(Array.Empty<DirectoryItem>(), getFiles.Path, 1, 2));
 
     public static  Task<IOResult> CopyItemsFromRemote(CopyItemsParam input)
-        => (new IOResult(null)).ToAsync();
+        => CopyItemsFromRemote(input.Path.GetIpAndPath(), input.TargetPath, input.Items, input.Move);
+
+    static Settings GetFiles(this IpAndPath ipAndPath) 
+        => GetFiles(ipAndPath.Ip, () => JsonContent.Create(new { Path = ipAndPath.Path }));
 
     static DirectoryItem ToDirectoryItem(this RemoteItem item)
         => new(item.Name, item.Size, item.IsDirectory, null, item.IsHidden, 
             DateTimeOffset.FromUnixTimeMilliseconds(item.Time).LocalDateTime);
     static GetFilesResult ToFilesResult(this DirectoryItem[] items, string path)
         => new GetFilesResult(items, path, items.Where(n => n.IsDirectory).Count(), items.Where(n => !n.IsDirectory).Count());
+
+    static IpAndPath GetIpAndPath(this string url)
+        => new(url.StringBetween('/', '/'), "/" + url.SubstringAfter('/').SubstringAfter('/'));
+
+    static  Task<IOResult> CopyItemsFromRemote(IpAndPath ipAndPath, string targetPath, CopyItem[] Items, bool Move)
+        => (new IOResult(null)).ToAsync();
 }
 
 record RemoteItem(
@@ -49,4 +58,9 @@ record RemoteItem(
     bool IsDirectory,
     bool IsHidden,
     long Time
+);
+
+record IpAndPath(
+    string Ip,
+    string Path
 );
