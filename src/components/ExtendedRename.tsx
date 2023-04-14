810d39e2eeccbf9be6a422c350f9232b7a5706d0
Uwe Riegel <uriegel@hotmail.de>
2023-04-14 08:02:30 +0200

    Saving extended rename data

--- src/components/ExtendedRename.tsx
+++ src/components/ExtendedRename.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import "../extensions/extensions"
 
 const ExtendedRename = () => {
@@ -7,31 +7,51 @@ const ExtendedRename = () => {
     const [digits, setDigits] = useState((localStorage.getItem("extendedRenameDigits") ?? "3"))
     const [startNumber, setStartNumber] = useState((localStorage.getItem("extendedRenameStartNumber") ?? "1"))
 
+    const startNumberRef = useRef<HTMLInputElement>(null)
+    useEffect(() => {
+        if (startNumberRef.current)
+            return () => {
+                localStorage.setItem("extendedRenamePrefix", prefix)
+                localStorage.setItem("extendedRenameDigits", digits)
+                localStorage.setItem("extendedRenameStartNumber", startNumber)
+            }
+    }, [prefix, digits, startNumber])
+
+    useEffect(() => {
+        if (startNumberRef.current)
+            startNumberRef.current.focus()
+    }, [startNumberRef])
+        
+    const selectInput = (e: React.FocusEvent<HTMLInputElement>) => e.target?.select()
+
     return (
         <table>
-            <tr>
-                <td className="right">Prefix:</td>
-                <td>
-                    <input className="wdr-focusable" type="text" value={prefix} onChange={e => setPrefix(e.target.value)}/>
-                </td>
-            </tr>
-            <tr>
-                <td className="right">Stellen:</td>
-                <td>
-                    <select value={digits} onChange={e => setDigits(e.target.value)} className="wdr-focusable">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td className="right">Start:</td>
-                <td>
-                    <input type="number" className="wdr-focusable" value={startNumber} onChange={e => setStartNumber(e.target.value)}/>
-                </td>
-            </tr>
+            <tbody>
+                <tr>
+                    <td className="right">Prefix:</td>
+                    <td>
+                        <input className="wdr-focusable" type="text" value={prefix} onChange={e => setPrefix(e.target.value)} onFocus={selectInput} />
+                    </td>
+                </tr>
+                <tr>
+                    <td className="right">Stellen:</td>
+                    <td>
+                        <select value={digits} onChange={e => setDigits(e.target.value)} className="wdr-focusable">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="right">Start:</td>
+                    <td>
+                        <input type="number" ref={startNumberRef} className="wdr-focusable" value={startNumber} onChange={e => setStartNumber(e.target.value)}
+                        onFocus={selectInput} />
+                    </td>
+                </tr>
+            </tbody>
         </table>
     )
 }
