4f685cf5b1390dca9bb07a41afb1e3809a21ec0a
Uwe Riegel <uriegel@hotmail.de>
2023-04-24 15:33:49 +0200

    Gtk4 Theme

--- Commander/platforms/Linux/Directory.cs
+++ Commander/platforms/Linux/Directory.cs
@@ -4,11 +4,12 @@ using Microsoft.AspNetCore.Http;
 
 using AspNetExtensions;
 using CsTools.Extensions;
-//using GtkDotNet;
+using GtkDotNet;
 using LinqTools;
 
 using static CsTools.Core;
 using CsTools;
+using System.Runtime.InteropServices;
 
 static partial class Directory
 {
@@ -16,28 +17,46 @@ static partial class Directory
         => info.Extension?.Length > 0 ? info.Extension : ".noextension";
 
     public static Task ProcessIcon(HttpContext context, string iconHint)
-        => RepeatOnException(async () => { }
-            // await WebWindowNetCore.WebView.GtkApplication!.Dispatch(async () =>
-            //     {
+        => RepeatOnException(async () => 
+            await Application.Dispatch(async () =>
+                {
             //         // using var iconInfo = IconInfo.Choose(iconHint, 16, IconLookup.ForceSvg);
+                    var type = Gtk.GuessContentType(iconHint);
+                    var icon = IconGet(type);
+                    var names = IconGetNames(icon);
+                    var handle = ChooseIcon(IntPtr.Zero, names, 16, IconLookup.ForceRegular);
             //         // var iconFile = iconInfo.GetFileName();
             //         // using var stream = iconFile?.OpenFile();
             //         // await context.SendStream(stream!, startTime, iconFile);
-            //    }, 100), 
-            , 1);
+               }, 100), 
+            1);
+
+    [DllImport("libgtk-4.so", EntryPoint="g_content_type_get_icon", CallingConvention = CallingConvention.Cdecl)]
+    extern static IntPtr IconGet(string contentType);
+    [DllImport("libgtk-4.so", EntryPoint="g_themed_icon_get_names", CallingConvention = CallingConvention.Cdecl)]
+    extern static IntPtr IconGetNames(IntPtr icon);
+    [DllImport("libgtk-4.so", EntryPoint="gtk_icon_theme_choose_icon", CallingConvention = CallingConvention.Cdecl)]
+    extern static IntPtr ChooseIcon(IntPtr theme, IntPtr iconNames, int size, IconLookup flags);
 
     public static Task<GetExtendedItemsResult> GetExtendedItems(GetExtendedItems getExtendedItems)
         => GetExtendedItems(getExtendedItems.Path, getExtendedItems.Items)
             .ToAsync();
 
-    public static async Task<IOResult> DeleteItems(DeleteItemsParam input)
-    =>  new IOResult(null);
-    // => WebWindowNetCore.WebView.GtkApplication!.Dispatch(() =>
-    //     {
-    //         input.Names.ForEach(n => GFile.Trash(input.Path.AppendPath(n)));
-    //         return new IOResult(null);
-    //     }, 100)
-    //         .Catch(MapExceptionToIOResult);
+    public static Task<IOResult> DeleteItems(DeleteItemsParam input)
+        => Application.Dispatch(() =>
+        {
+            input.Names.ForEach(Trash);
+            return new IOResult(null);
+
+            void Trash(string name)
+            {
+                var file = GFile.New(input.Path.AppendPath(name));
+                var error = IntPtr.Zero;
+                GFile.Trash(file, IntPtr.Zero, ref error);
+                GObject.Unref(file);
+            }
+        }, 100)
+            .Catch(MapExceptionToIOResult);
 
     static void CopyItem(string name, string path, string targetPath, Action<long, long> progress, bool move, CancellationToken cancellationToken) { }
     // => Copy(path.AppendPath(name), targetPath.AppendPath(name), FileCopyFlags.Overwrite,
@@ -56,8 +75,8 @@ static partial class Directory
         => e switch
         {
             UnauthorizedAccessException ue                     => IOError.AccessDenied,
-            //GtkDotNet.GErrorException gee  when gee.Code ==  1 => IOError.FileNotFound, 
-            //GtkDotNet.GErrorException gee  when gee.Code == 14 => IOError.AccessDenied,
+            GtkDotNet.GErrorException gee  when gee.Code ==  1 => IOError.FileNotFound, 
+            GtkDotNet.GErrorException gee  when gee.Code == 14 => IOError.AccessDenied,
             _                                                  => IOError.Exn
         };
 
