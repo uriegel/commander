bb34e1ae2800b78802ac794db67c3b18b6eaed95
Uwe Riegel <uriegel@hotmail.de>
2023-10-28 17:40:12 +0200

    Copy now possible in Windows

--- Commander/platforms/Windows/Extensions.cs
+++ Commander/platforms/Windows/Extensions.cs
@@ -3,5 +3,10 @@ using LinqTools;
 static class StringExtensions
  {
     public static string RemoveWriteProtection(this string file)
-        => file.SideEffect(n =>  (new FileInfo(n)).IsReadOnly = false);
+        => file.SideEffect(n => {
+            var fi = new FileInfo(n);
+            if (fi.Exists)
+                fi.IsReadOnly = false;
+        });
+         
  }
