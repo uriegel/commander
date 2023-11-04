6a719f5e0e0e16f67ba29795a4a2df0f06b4b232
Uwe Riegel <uriegel@hotmail.de>
2023-11-04 11:47:39 +0100

    Get service description

--- Commander/platforms/Windows/Services.cs
+++ Commander/platforms/Windows/Services.cs
@@ -1,5 +1,6 @@
 #if Windows
 
+using System.Runtime.InteropServices;
 using System.ServiceProcess;
 using LinqTools;
 
@@ -19,10 +20,64 @@ static class Services
                 .Select(ServiceItem.Create)
                 .ToArray()
                 .ToAsync();
+
+    public static void Mist()
+    {
+        var schSCManager = OpenSCManager(null, null, SERVICE_QUERY_CONFIG);
+        var schService = OpenService(schSCManager, "Power", SERVICE_QUERY_CONFIG);
+        bool success = QueryServiceConfig2(schService, ServiceConfig.DESCRIPTION, IntPtr.Zero, 0, out var bytesNeeded);
+        if (!success && Marshal.GetLastWin32Error() == ERROR_INSUFFICIENT_BUFFER)
+        {
+
+            var buffer = Marshal.AllocHGlobal((int)bytesNeeded);
+            bool success2 = QueryServiceConfig2(schService, ServiceConfig.DESCRIPTION, buffer, bytesNeeded, out bytesNeeded);
+            if (!success2)
+                return;
+            var description = (SERVICE_DESCRIPTION)Marshal.PtrToStructure(buffer, typeof(SERVICE_DESCRIPTION));
+            Marshal.FreeHGlobal(buffer);
+            CloseServiceHandle(schService);
+            CloseServiceHandle(schSCManager);
+        }
+
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct SERVICE_DESCRIPTION
+    {
+        public string lpDescription;
+    }
+
+    [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    static extern IntPtr OpenSCManager(string? lpMachineName, string? lpDatabaseName, uint dwDesiredAccess);
+
+    [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    static extern IntPtr OpenService(IntPtr hSCManager, string lpServiceName, uint dwDesiredAccess);
+
+    [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    static extern bool QueryServiceConfig2(IntPtr hService, ServiceConfig dwInfoLevel, IntPtr lpBuffer, uint cbBufSize, out uint pcbBytesNeeded);
+
+    [DllImport("advapi32.dll", SetLastError = true)]
+    static extern bool CloseServiceHandle(IntPtr hSCObject);
+
+    const uint SERVICE_QUERY_CONFIG = 0x0001;
+    const uint ERROR_INSUFFICIENT_BUFFER = 122;
+
+    public enum ServiceConfig 
+    {
+        DESCRIPTION = 1,
+        FAILURE_ACTIONS,
+        DELAYED_AUTO_START_INFO,
+        FAILURE_ACTIONS_FLAG,
+        SERVICE_SID_INFO,
+        REQUIRED_PRIVILEGES_INFO,
+        PRESHUTDOWN_INFO,
+        TRIGGER_INFO,
+        PREFERRED_NODE,
+        LAUNCH_PROTECTED = 12
+    }
 }
 
 #endif
-
 
 
     /*
@@ -39,9 +94,6 @@ using System;
 
     #region "Constants"
 
-    const uint ERROR_INSUFFICIENT_BUFFER = 122;
-
-    const uint SERVICE_QUERY_CONFIG = 0x0001;
     const uint STANDARD_RIGHTS_REQUIRED = 0x000F0000;
     const uint SC_MANAGER_CONNECT = 0x0001;
     const uint SC_MANAGER_CREATE_SERVICE = 0x0002;
@@ -51,111 +103,6 @@ using System;
     const uint SC_MANAGER_MODIFY_BOOT_CONFIG = 0x0020;
     const uint SC_MANAGER_ALL_ACCESS = STANDARD_RIGHTS_REQUIRED | SC_MANAGER_CONNECT | SC_MANAGER_CREATE_SERVICE | SC_MANAGER_ENUMERATE_SERVICE | SC_MANAGER_LOCK | SC_MANAGER_QUERY_LOCK_STATUS | SC_MANAGER_MODIFY_BOOT_CONFIG;
 
-    #endregion
-
-    #region "Win32 APIs"
-
-    [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    static extern IntPtr OpenSCManager(string lpMachineName, string lpDatabaseName, uint dwDesiredAccess);
-
-    [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    static extern IntPtr OpenService(IntPtr hSCManager, string lpServiceName, uint dwDesiredAccess);
-
-    [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    static extern bool QueryServiceConfig2(IntPtr hService, SERVICE_CONFIG dwInfoLevel, IntPtr lpBuffer, uint cbBufSize, out uint pcbBytesNeeded);
-
-    [DllImport("advapi32.dll", SetLastError = true)]
-    static extern bool CloseServiceHandle(IntPtr hSCObject);
-
-    #endregion
-
-    #region "Enumerations"
-
-    public enum SERVICE_CONFIG : uint
-    {
-        SERVICE_CONFIG_DESCRIPTION = 1,
-        SERVICE_CONFIG_FAILURE_ACTIONS = 2,
-        SERVICE_CONFIG_DELAYED_AUTO_START_INFO = 3,
-        SERVICE_CONFIG_FAILURE_ACTIONS_FLAG = 4,
-        SERVICE_CONFIG_SERVICE_SID_INFO = 5,
-        SERVICE_CONFIG_REQUIRED_PRIVILEGES_INFO = 6,
-        SERVICE_CONFIG_PRESHUTDOWN_INFO = 7,
-        SERVICE_CONFIG_TRIGGER_INFO = 8,
-        SERVICE_CONFIG_PREFERRED_NODE = 9,
-        SERVICE_CONFIG_LAUNCH_PROTECTED = 12
-    }
-
-    #endregion
-
-    #region "Structures"
-
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-    public struct SERVICE_DESCRIPTION
-    {
-        public string lpDescription;
-    }
-
-    #endregion
-
-    static void Main(string[] args)
-    {
-
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-
-        string svcName = "Power"; // Change this to the service name you want to get its description
-
-        // Get the SCM database handle
-        IntPtr schSCManager = OpenSCManager(null, null, SERVICE_QUERY_CONFIG);
-
-        // Get the service handle
-
-        IntPtr schService = OpenService(schSCManager, svcName, SERVICE_QUERY_CONFIG);
-        if (schService == IntPtr.Zero)
-        {
-            Console.WriteLine("Win32 API OpenService failed ({0})", Marshal.GetLastWin32Error());
-            return;
-        }
-
-        // Query the service description
-        IntPtr buffer;
-
-        bool success = QueryServiceConfig2(schService, SERVICE_CONFIG.SERVICE_CONFIG_DESCRIPTION, IntPtr.Zero, 0, out uint bytesNeeded);
-
-        if (!success && Marshal.GetLastWin32Error() == ERROR_INSUFFICIENT_BUFFER)
-        {
-
-            buffer = Marshal.AllocHGlobal((int)bytesNeeded);
-            bool success2 = QueryServiceConfig2(schService, SERVICE_CONFIG.SERVICE_CONFIG_DESCRIPTION, buffer, bytesNeeded, out bytesNeeded);
-
-            if (!success2)
-            {
-                Console.WriteLine("Win32 API QueryServiceConfig2 failed ({0})", Marshal.GetLastWin32Error());
-                return;
-            }
-
-        }
-        else
-        {
-            Console.WriteLine("Win32 API QueryServiceConfig2 failed ({0})", Marshal.GetLastWin32Error());
-            return;
-        }
-
-        // Read the description from the buffer
-        SERVICE_DESCRIPTION description = (SERVICE_DESCRIPTION)Marshal.PtrToStructure(buffer, typeof(SERVICE_DESCRIPTION));
-        Console.WriteLine("{0} service description: {1}", svcName, description.lpDescription);
-
-        // Free the buffer and close the handles
-        Marshal.FreeHGlobal(buffer);
-        CloseServiceHandle(schService);
-        CloseServiceHandle(schSCManager);
-
-        stopwatch.Stop();
-        Console.WriteLine("Elapsed Time: {0} Milliseconds", stopwatch.ElapsedMilliseconds.ToString());
-
-        Console.ReadLine();
-
-    }    
 
 public static Item CreateServiceItem(ServiceController sc)
     {
