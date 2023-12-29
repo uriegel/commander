7a78ff7ccf75f787d2977b88157dc701fc355ca7
Uwe Riegel <uriegel@hotmail.de>
2023-12-29 08:49:00 +0100

    Test FileSystemWatcher

--- Commander/Program.cs
+++ Commander/Program.cs
@@ -10,6 +10,22 @@ if (args.Length > 0 && args[0] == "-adminMode")
         
 #endif
 
+var fsw = new FileSystemWatcher("/media/uwe/Video")
+{
+    NotifyFilter = NotifyFilters.CreationTime
+                    | NotifyFilters.DirectoryName
+                    | NotifyFilters.FileName
+                    | NotifyFilters.LastWrite
+                    | NotifyFilters.Size,
+    EnableRaisingEvents = true,
+    IncludeSubdirectories = true
+};
+//fsw.Changed += (s, e) => Console.WriteLine($"Changed {e.Name} {e.}");
+fsw.Created += (s, e) => Console.WriteLine($"Created {e.Name}");
+fsw.Deleted += (s, e) => Console.WriteLine($"Deleted {e.Name}");
+fsw.Renamed += (s, e) => Console.WriteLine($"Renamed {e.OldName}, {e.Name}");
+
+
 Window.Run();
 
 
