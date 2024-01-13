ac60725b4037952ae6416d7d55a9bbb16dd27ac5
Uwe Riegel <uriegel@hotmail.de>
2024-01-13 09:35:39 +0100

    Favorites

--- src/controller/favorites.tsx
+++ src/controller/favorites.tsx
@@ -1,7 +1,7 @@
 import { DialogHandle, ResultType } from "web-dialog-react"
 import { FolderViewItem } from "../components/FolderView"
 import IconName from "../components/IconName"
-import { Controller, ControllerResult, ControllerType, EnterData, OnEnterResult } from "./controller"
+import { Controller, ControllerResult, ControllerType, EnterData, OnEnterResult, addParent } from "./controller"
 import { ROOT } from "./root"
 import { IconNameType } from "../enums"
 import { AsyncResult, Err, ErrorType, Nothing, Ok, nothing } from "functional-extensions"
@@ -35,10 +35,10 @@ const getFavoriteItems = () => {
     return itemsStr ? JSON.parse(itemsStr) as FolderViewItem[] : []
 }
 
-const showAddFavorite = async (dialog?: DialogHandle | null, otherPath?: string) => {
+const showAddFavorite = async (dialog: DialogHandle, otherPath?: string) => {
     const items = getFavoriteItems()
     const result =
-        !items.find(n => n.name == otherPath) && (await dialog?.show({
+        !items.find(n => n.name == otherPath) && (await dialog.show({
             text: `'${otherPath}' als Favoriten hinzufügen?`,
             btnOk: true,
             btnCancel: true,
@@ -53,7 +53,7 @@ const showAddFavorite = async (dialog?: DialogHandle | null, otherPath?: string)
         return false
 }
 
-const onNew = (dialog?: DialogHandle|null, refresh?: ()=>void, otherPath?: string) => {
+const onNew = (dialog: DialogHandle, refresh?: ()=>void, otherPath?: string) => {
     const show = async () => {
         if (refresh && await showAddFavorite(dialog, otherPath))
             refresh()
@@ -95,40 +95,39 @@ export const getFavoritesController = (controller: Controller | null): Controlle
         extendedRename: () => AsyncResult.from(new Err<Controller, Nothing>(nothing)),
         renameAsCopy: () => AsyncResult.from(new Ok<Nothing, ErrorType>(nothing)),
         createFolder: () => AsyncResult.from(new Ok<string, ErrorType>("")),
-        deleteItems: () => AsyncResult.from(new Ok<Nothing, ErrorType>(nothing)),
-        // TODOdeleteItems,
+        deleteItems,
         onSelectionChanged: () => { },
         cleanUp: () => { }
     }})
 
-    // TODO
-    const getItems = () => AsyncResult.from(new Err<GetItemsResult, ErrorType>({status: IOError.Canceled, statusText: ""}))
-// const getItems = async () => {
-//     const items = getFavoriteItems()
-//     return {
-//         path: FAVORITES,
-//         dirCount: items.length,
-//         fileCount: 0,
-//         error: IOError.NoError,
-//         items: addParent(items)
-//                 .concat({
-//                     name: "Favoriten hinzufügen...",
-//                     isNew: true
-//                 })
-//    }
-//}
+const getItems = () => {
+    const items = getFavoriteItems()
+    return AsyncResult
+        .from(new Ok<GetItemsResult, ErrorType>({
+            path: FAVORITES,
+            dirCount: items.length,
+            fileCount: 0,
+            items: addParent(items)
+                .concat({
+                    name: "Favoriten hinzufügen...",
+                    isNew: true
+                })
+        }
+        ))
+}
 
-// const deleteItems = async (_: string, items: FolderViewItem[], dialog: DialogHandle | null) => {
-// 	const result = await dialog?.show({
-// 		text: `Möchtest Du ${items.length > 1 ? "die Favoriten" : "den Favoriten"} löschen?`,
-// 		btnOk: true,
-// 		btnCancel: true,
-// 		defBtnOk: true
-// 	})
-//     if (result?.result == ResultType.Ok) {
-//         const favs = getFavoriteItems().filter(x => !items.find(n => n.name == x.name))
-//         localStorage.setItem("fav", JSON.stringify(favs))
-//     }
-//     return null
-// }
+const deleteItems = (_: string, items: FolderViewItem[], dialog: DialogHandle) => 
+    dialog?.showDialog<Nothing, ErrorType>({
+		text: `Möchtest Du ${items.length > 1 ? "die Favoriten" : "den Favoriten"} löschen?`,
+		btnOk: true,
+		btnCancel: true,
+		defBtnOk: true
+    }, res => {
+        if (res.result == ResultType.Ok) {
+            const favs = getFavoriteItems().filter(x => !items.find(n => n.name == x.name))
+            localStorage.setItem("fav", JSON.stringify(favs))
+        }
+        return new Ok<Nothing, ErrorType>(nothing)
+    })
+
 
