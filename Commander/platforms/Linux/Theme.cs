9cdf80b4e31049b18bff298c3f4e7db9a0cb3b7b
Uwe Riegel <uriegel@hotmail.de>
2024-04-28 10:29:11 +0200

    Range 1..^1

--- Commander/platforms/Linux/Theme.cs
+++ Commander/platforms/Linux/Theme.cs
@@ -7,12 +7,7 @@ static class Theme
 {
     public static readonly string BaseTheme;
     
-    public static string GetThemeName(this string osTheme)
-        => osTheme.Contains("-dark")
-            ? "adwaitaDark"
-            : "adwaita";
-
-    public static string Get()
+    static string Get()
     {
         static string GetKdeTheme()
         {
@@ -52,39 +47,42 @@ static class Theme
         }
 
         static string GetGnomeTheme()
-            => Gtk.Dispatch(() => 
-                GtkSettings.GetDefault()
-                    .GetString("gtk-theme-name"))
-                .Result ?? "";
+        {
+            var output = "";
+            using var proc = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    FileName = "gsettings",
+                    CreateNoWindow = true,
+                    Arguments = "get org.gnome.desktop.interface gtk-theme",
+                },
+                EnableRaisingEvents = true
+            };
+            proc.OutputDataReceived += (s, e) =>
+            {
+                if (e.Data != null)
+                    output = e.Data;
+            };
+            proc.ErrorDataReceived += (s, e) => Console.Error.WriteLine(e.Data);
+            proc.Start();
+            proc.BeginOutputReadLine();
+            proc.BeginErrorReadLine();
+            proc.EnableRaisingEvents = true;
+            proc.WaitForExit();
+            return output;
+        }
 
         return Platform.Value switch
-            {
-                PlatformType.Kde => GetKdeTheme(),
-                _ => GetGnomeTheme()
-            };
-        }
-
-    public static void StartThemeDetection(Action<string> onChanged)
-    {
-        void StartKdeThemeDetection() { }
-
-        async void StartGnomeThemeDetection()
         {
-            await Gtk.Dispatch(() =>
-                GtkSettings
-                    .GetDefault()
-                    .OnNotify("gtk-theme-name", s =>
-                        onChanged(s.GetString("gtk-theme-name")
-                                    ?.GetThemeName() ?? "")));
-        }
-
-        if (Platform.Value == PlatformType.Kde)
-            StartKdeThemeDetection();
-        else
-            StartGnomeThemeDetection();
+            PlatformType.Kde => GetKdeTheme(),
+            _ => GetGnomeTheme()
+        };
     }
 
     static Theme()
-    => BaseTheme = Get().SubstringUntil('-');
+        => BaseTheme = Get()[1..^1].SubstringUntil('-');
 }
 #endif
