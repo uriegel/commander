edd6fdb4612a80aea169ead2501328ae64a023ec
Uwe Riegel <uriegel@hotmail.de>
2024-05-11 15:31:33 +0200

    slide track

--- Commander/TrackInfo.cs
+++ Commander/TrackInfo.cs
@@ -1,20 +1,30 @@
-using System.Text.Json;
 using System.Xml.Serialization;
 using CsTools.Functional;
 using CsTools.HttpRequest;
 using static CsTools.Core;
 
-record TrackInfoData();
+record TrackInfoData(
+    string? Name,
+    string? Description,
+    TrackPoint[]? TrackPoints
+);
 
+record TrackPoint(
+    double Latitude,
+    double Longitude,
+    double Elevation,
+    string? Time
+);
 
-[XmlRoot(ElementName = "gpx", Namespace = "http://www.topografix.com/GPX/1/0")]
-public class GpxInfo
+//[XmlRoot(ElementName = "gpx", Namespace = "http://www.topografix.com/GPX/1/0")]
+[XmlRoot(ElementName = "gpx")]
+public class XmlTrackInfo
 {
     [XmlElement("trk")]
-    public Track? Track;
+    public XmlTrack? Track;
 }
 
-public class Track
+public class XmlTrack
 {
     [XmlElement("name")]
     public string? Name;
@@ -23,16 +33,16 @@ public class Track
     public string? Description;
 
     [XmlElement("trkseg")]
-    public TrackSegment? TrackSegment;
+    public XmlTrackSegment? TrackSegment;
 }
 
-public class TrackSegment
+public class XmlTrackSegment
 {
     [XmlElement("trkpt")]
-    public TrackPoint[]? TrackPoints;
+    public XmlTrackPoint[]? TrackPoints;
 }
 
-public class TrackPoint
+public class XmlTrackPoint
 {
     [XmlAttribute("lat")]
     public double Latitude;
@@ -43,7 +53,7 @@ public class TrackPoint
     public double Elevation;
 
     [XmlElement("time")]
-    public DateTime Time;
+    public string? Time;
 }
 
 
@@ -51,11 +61,18 @@ static class TrackInfo {
     public static AsyncResult<TrackInfoData, RequestError> Get(GetTrackInfoParam param) 
     {
 
-        var serializer = new XmlSerializer(typeof(GpxInfo));
+        var serializer = new XmlSerializer(typeof(XmlTrackInfo));
         using var stream = File.OpenRead(param.Path);
-        var obj = serializer.Deserialize(stream) as GpxInfo;
-        var json = JsonSerializer.Serialize<GpxInfo>(obj!);
-
-        return Ok<TrackInfoData, RequestError>(new TrackInfoData()).ToAsyncResult();
+        var xmlTrackInfo = serializer.Deserialize(stream) as XmlTrackInfo;
+        var trackInfo = new TrackInfoData(
+            xmlTrackInfo?.Track?.Name, 
+            xmlTrackInfo?.Track?.Description, 
+            xmlTrackInfo
+                ?.Track
+                ?.TrackSegment
+                ?.TrackPoints
+                ?.Select(n => new TrackPoint(n.Latitude, n.Longitude, n.Elevation, n.Time))
+                .ToArray());
+        return Ok<TrackInfoData, RequestError>(trackInfo).ToAsyncResult();
     }
 }
--- src/components/TrackViewer.tsx
+++ src/components/TrackViewer.tsx
@@ -1,35 +1,57 @@
 import 'leaflet/dist/leaflet.css'
-import { map, tileLayer, Map as LMap, GPX } from "leaflet"
+import { map, tileLayer, Map as LMap, GPX, Marker, Icon, marker } from "leaflet"
 import "leaflet-gpx"
 import useResizeObserver from '@react-hook/resize-observer'
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import './TrackViewer.css'
 import { getViewerPath } from '../controller/controller'
 import { ErrorType, jsonPost } from 'functional-extensions'
 
 type TrackInfo = {
+    name?: string
+    description?: string
+    trackPoints?: TrackPoint[] 
+}
 
+type TrackPoint = {
+    latitude?: number
+    longitude?: number
+    elevation?: number
+    time?: Date
 }
 
 type TrackViewerProps = {
     path: string
 }
 
+const myIcon = new Icon.Default({ imagePath: "http://localhost:20000/static/"})
+
 const TrackViewer = ({ path }: TrackViewerProps) => {
 
     const first = useRef(false) 
     const resizeTimer = useRef(0) 
+    const locationMarker = useRef<Marker<any>|null>(null)
     const myMap = useRef<LMap | null>(null)
-
     const gpx = useRef<GPX|null>(null)
-
     const root = useRef<HTMLDivElement>(null)
+    const trackInfo = useRef<TrackInfo|null>(null)
+    const [pointCount, setPointCount] = useState(0)
+    const [position, setPosition] = useState(0)
+
     useResizeObserver(root, () => {
         clearTimeout(resizeTimer.current)
         resizeTimer.current = setTimeout(() => {
             myMap.current?.invalidateSize({ debounceMoveend: true, animate: true })    
         }, 400);
     })
+
+    useEffect(() => {
+        locationMarker.current?.remove()
+        if (trackInfo.current?.trackPoints && trackInfo.current.trackPoints[position].latitude && trackInfo.current.trackPoints![position].longitude) {
+            locationMarker.current = marker([trackInfo.current.trackPoints[position].latitude!, trackInfo.current.trackPoints[position].longitude!],
+                { icon: myIcon }).addTo(myMap.current!)
+        }
+    }, [position])
 
     useEffect(() => {
         if (!first.current) {
@@ -50,12 +72,22 @@ const TrackViewer = ({ path }: TrackViewerProps) => {
         }).on('loaded', e => {
             myMap.current?.fitBounds(e.target.getBounds())
         }).addTo(myMap.current!)
-        jsonPost<TrackInfo, ErrorType>({ method: "gettrackinfo", payload: { path } })		
+        jsonPost<TrackInfo, ErrorType>({ method: "gettrackinfo", payload: { path } })
+            .match(n => {
+                trackInfo.current = n
+                setPosition(0)
+                setPointCount(n.trackPoints?.length ?? 0)
+            }, e => console.error(e))
     }, [path])
 
-    return <div className="trackView" ref={root}>
-        <div id="map" className="trackMap"></div>
+    return <div className="trackViewContainer">
+        <div className="trackView" ref={root}>
+            <div id="map" className="trackMap"></div>
+        </div>
+        <input type="range" min="1" max={pointCount} value={position} onChange={n => setPosition(Number.parseInt(n.target.value)-1)}></input>
     </div>
+
+    
 }
 
 export default TrackViewer
