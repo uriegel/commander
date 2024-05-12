48c22cb128c9331fab54d215ab5ad676027f3992
Uwe Riegel <uriegel@hotmail.de>
2024-05-12 13:42:00 +0200

    react-leaflet

--- src/components/LocationViewer.tsx
+++ src/components/LocationViewer.tsx
@@ -1,54 +1,40 @@
 import 'leaflet/dist/leaflet.css'
-import { map, marker, tileLayer, Map as LMap, Marker, Icon } from "leaflet"
-import useResizeObserver from '@react-hook/resize-observer'
-import { useEffect, useRef } from "react"
 import './LocationViewer.css'
+import useResizeObserver from '@react-hook/resize-observer'
+import { Map as LMap } from "leaflet"
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { useEffect, useRef } from 'react'
 
 type LocationViewerProps = {
-    latitude?: number
-    longitude?: number
+    latitude: number
+    longitude: number
 }
 
-const myIcon = new Icon.Default({ imagePath: "http://localhost:20000/static/"})
+const LocationViewer = ({ latitude, longitude }: LocationViewerProps) => {
 
-const LocationViewer = ({latitude, longitude }: LocationViewerProps) => {
-
-    const first = useRef(false) 
-    const resizeTimer = useRef(0) 
     const myMap = useRef<LMap | null>(null)
-    const locationMarker = useRef<Marker<any>|null>(null)
-
     const root = useRef<HTMLDivElement>(null)
-    useResizeObserver(root, () => {
-        clearTimeout(resizeTimer.current)
-        resizeTimer.current = setTimeout(() => {
-            myMap.current?.invalidateSize({ debounceMoveend: true, animate: true })    
-        }, 400);
-    })
 
     useEffect(() => {
-        if (first.current && longitude && latitude && myMap.current) {
-            myMap.current?.setView([latitude, longitude], 13)
-            locationMarker.current?.remove()
-            locationMarker.current = marker([latitude, longitude], {icon: myIcon}).addTo(myMap.current)
-        }
+        myMap.current?.setView([latitude!, longitude!])
     }, [latitude, longitude])
 
-    useEffect(() => {
-        if (!first.current && longitude && latitude) {
-            first.current = true
-            myMap.current = map('map').setView([latitude, longitude], 13)
-            tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
-                maxZoom: 19
-            }).addTo(myMap.current)
-            locationMarker.current?.remove()
-            locationMarker.current = marker([latitude, longitude], {icon: myIcon}).addTo(myMap.current)
-        }
-    }, [longitude, latitude])
+    useResizeObserver(root.current, () => {
+        myMap.current?.invalidateSize({ debounceMoveend: true, animate: true })    
+    })
 
-    return <div className="locationView" ref={root}>
-        <div id="map" className="locationMap"></div>
-    </div>
+    return (
+        <div className="locationView" ref={root}>
+            <MapContainer className='location' ref={myMap} center={[latitude, longitude]} zoom={13} scrollWheelZoom={true} >
+                <TileLayer attribution='' url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png" />
+                <Marker position={[latitude, longitude]}>
+                    <Popup>Der Aufnahmestandort.</Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    )
 }
 
 export default LocationViewer
+
+
