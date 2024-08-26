8d074d589ff31e4cd6cee8228a175caa2fc2b393
uriegel <uriegel@hotmail.de>
2024-08-26 19:36:40 +0200

    Commander with web view

--- Commander/Program.fs
+++ Commander/Program.fs
@@ -1,2 +1,21 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.Drawing
+open WebWindowNetCore
+
+WebView()
+    .AppId("de.uriegel.commander")
+    .InitialBounds(600, 800)
+    .Title("Commander")
+    .BackgroundColor(Color.Transparent)
+    .ResourceIcon("icon")
+    .ResourceFromHttp()
+    .DebugUrl("http://localhost:5173")
+    .CorsDomains([|"http://localhost:5173"|])
+    .CorsCache(TimeSpan.FromSeconds(20))    
+    .SaveBounds()
+    .DefaultContextMenuDisabled()
+#if DEBUG    
+    .DevTools()
+#endif
+    .Run()
+    |> ignore
