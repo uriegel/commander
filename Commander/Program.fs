09de8d616183423c3c9b48a6e7a667b011612db2
Uwe Riegel <uriegel@hotmail.de>
2024-09-10 00:48:38 +0200

    TODO

--- Commander/Program.fs
+++ Commander/Program.fs
@@ -8,11 +8,4 @@ let main (args : string[]) =
         Elevated.runServer args[0]
         0
     
-// TODO Delete: mapError (Access denied)
 // TODO CreateDir
-
-// TODO UAC: call Commander with params:
-//              start program (Windows shellexecute, Linux runCmd)    
-//              GetParams from binary utf8 stream (Windows?)
-//              GetResult to binary utf8 stream
-//              GetEvents to binary utf8 stream
