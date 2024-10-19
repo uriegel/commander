0fd68dbf9560a7ff7248c88f3c30c92a250cc1fc
Uwe Riegel <uriegel@hotmail.de>
2024-10-19 14:10:14 +0200

    Reapply "With crate windows.rs"
    This reverts commit 5d4328e6983b8ce51e4831edcc701541a65507da.

--- src/windows/root.rs
+++ src/windows/root.rs
@@ -1,18 +1,13 @@
-use std::{mem, ptr::null_mut, slice::from_raw_parts};
-
 use serde::Serialize;
-use winapi::um::{
-    errhandlingapi::GetLastError, 
-    handleapi::{INVALID_HANDLE_VALUE, CloseHandle}, 
-    fileapi::{
-        CreateFileW, GetDiskFreeSpaceExW, GetDriveTypeW, GetLogicalDriveStringsW, GetVolumeInformationW, OPEN_EXISTING}, ioapiset::DeviceIoControl, 
-        winioctl::FSCTL_IS_VOLUME_MOUNTED, 
-        winnt::{
-            FILE_SHARE_DELETE, FILE_SHARE_READ, FILE_SHARE_WRITE, GENERIC_READ, ULARGE_INTEGER
+use windows::{
+    core::PCWSTR, Win32::{
+        Foundation::{GENERIC_READ, HANDLE}, Storage::FileSystem::{
+            GetDiskFreeSpaceExW, GetDriveTypeW, GetLogicalDriveStringsW, GetVolumeInformationW, FILE_SHARE_DELETE, FILE_SHARE_READ, FILE_SHARE_WRITE, OPEN_EXISTING
         }
-    };
+    }
+};
 
-use crate::{requests::ItemsResult, windows::{pwstr_to_string, to_wstring}};
+use crate::requests::ItemsResult;
 
 #[derive(Debug)]
 #[derive(Copy, Clone, PartialEq)]
@@ -29,9 +24,8 @@ enum DriveType {
 #[serde(rename_all = "camelCase")]
 pub struct RootItem {
     pub name: String,
-    pub size: usize,
+    pub size: u64,
     pub description: String,
-    pub mount_point: String,
     pub is_mounted: bool,
     pub drive_type: String,
 }
@@ -43,34 +37,56 @@ pub struct ErrorItem {
     pub kind: i32
 }
 
-pub fn get_root()->ItemsResult<Vec<RootItem>> {
-    unsafe {
-        let mut buffer: Vec<u16> = Vec::with_capacity(500);
-        let size = GetLogicalDriveStringsW(500, buffer.as_mut_ptr());
-        let array: &[u16] = from_raw_parts(buffer.as_mut_ptr(), size as usize);
-        let drives_string = String::from_utf16_lossy(array);
-        let drives: Vec<&str> = drives_string.split("\0").collect();
+pub fn string_to_pcwstr(x: &str) -> Vec<u16> {
+    x.encode_utf16().chain(std::iter::once(0)).collect()
+}
 
-        unsafe fn get_drive_description(item: &str) -> String {
-            let path_ws = to_wstring(item);
-            let mut buffer: Vec<u16> = Vec::with_capacity(500);
-            match GetVolumeInformationW(path_ws.as_ptr(), buffer.as_mut_ptr(), 500, 
-                null_mut(), null_mut(), null_mut(), null_mut(), 0) {
-                0 => String::from(""),
-                _ => pwstr_to_string(buffer.as_mut_ptr())
+fn string_from_pcwstr(pwcstr: &[u16]) -> String {
+    let pstr: Vec<u16> = 
+        pwcstr
+            .iter()
+            .take_while(|&&i|i != 0)
+            .cloned()
+            .collect();
+    String::from_utf16_lossy(&pstr)
+}
+
+pub fn get_root()->ItemsResult<Vec<RootItem>> {
+    let mut drives: [u16; 512] = [0; 512];
+    let len = unsafe { GetLogicalDriveStringsW(Some(&mut drives)) };
+    let drives_string = String::from_utf16_lossy(&drives[..len as usize]);
+    let drives: Vec<&str> = drives_string
+        .split("\0")
+        .take_while(|d|*d != "")
+        .collect();
+
+        fn get_drive_description(drive: &str) -> String {
+            unsafe { 
+                let drive = string_to_pcwstr(&drive);
+                let mut desc_buffer = [0u16; 256];
+                GetVolumeInformationW(
+                    PCWSTR(drive.as_ptr()), 
+                    Some(&mut desc_buffer), 
+                    None, 
+                    None,
+                    None,
+                    None)
+                .ok()
+                .map(|_|string_from_pcwstr(&desc_buffer))
+                .unwrap_or("".to_string())
             }
         }
-
-        unsafe fn get_volume_size(item: &str) -> u64 {
-            let path_ws = to_wstring(item);
-            let mut ul: ULARGE_INTEGER = { mem::zeroed() };
-            let _ = GetDiskFreeSpaceExW(path_ws.as_ptr(), null_mut(), &mut ul, null_mut());
-            *ul.QuadPart()
+        
+        fn get_volume_size(drive: &str) -> u64 {
+            let drive = string_to_pcwstr(&drive);
+            let mut size: u64 = 0;
+            let _ = unsafe { GetDiskFreeSpaceExW(PCWSTR(drive.as_ptr()), None, Some(&mut size), None) };
+            size 
         }
 
-        unsafe fn get_drive_type(item: &str) -> DriveType {
-            let path_ws = to_wstring(item);
-            match GetDriveTypeW(path_ws.as_ptr()) {
+        fn get_drive_type(drive: &str) -> DriveType {
+            let drive = string_to_pcwstr(&drive);
+            match unsafe { GetDriveTypeW(PCWSTR(drive.as_ptr())) } {
                 2 => DriveType::REMOVABLE,
                 3 => DriveType::HARDDRIVE,
                 4 => DriveType::NETWORK,
@@ -79,41 +95,39 @@ pub fn get_root()->ItemsResult<Vec<RootItem>> {
             }
         }
 
-        unsafe fn is_mounted(item: &str) -> bool {
-            let volume = format!("\\\\.\\{}", item[0..2].to_string());
-            let path_ws = to_wstring(&volume);
-            let handle = CreateFileW(path_ws.as_ptr(), GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, null_mut(), 
-                OPEN_EXISTING, 0, null_mut());
-            if handle != INVALID_HANDLE_VALUE {
-                let result = DeviceIoControl(handle, FSCTL_IS_VOLUME_MOUNTED, null_mut(), 0, null_mut(), 
-                    0, null_mut(), null_mut());
-                CloseHandle(handle);
-                result != 0
-            } else {
-//         		// 2 means "no disk", anything else means by inference "disk
-// 	            // in drive, but you do not have admin privs to do a
-// 	            // CreateFile on that volume".
-                GetLastError() != 2
+//         fn is_mounted(item: &str) -> bool {
+//             let volume = format!("\\\\.\\{}", drive[0..2].to_string());
+//             let volume = string_to_pcwstr(&volume);
+//             unsafe {
+//                 let handle = CreateFileW(PCWSTR(volume.as_ptr()), GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, None, 
+//                     OPEN_EXISTING, 0, None);
+//                 if handle != INVALID_HANDLE_VALUE {
+//                     let result = DeviceIoharley
+//                     Control(handle, FSCTL_IS_VOLUME_MOUNTED, null_mut(), 0, null_mut(), 
+//                         0, null_mut(), null_mut());
+//                     CloseHandle(handle);
+//                     result != 0
+//                 } else {
+// //         		// 2 means "no disk", anything else means by inference "disk
+// // 	            // in drive, but you do not have admin privs to do a
+// // 	            // CreateFile on that volume".
+//                 GetLastError() != 2
+//                 }
+//             }
+//         }
+        
+    ItemsResult {
+        ok: drives.iter().map(|&item| {
+            let name = item.to_string();
+            let drive_type = get_drive_type(&name);
+            RootItem { 
+                name: name.clone(),
+                description: get_drive_description(&name),
+                size: get_volume_size(&name),
+                drive_type: "".to_string(),
+                is_mounted: true //if drive_type == DriveType::HARDDRIVE { true} else { is_mounted(&name) }
             }
-        }
-
-        ItemsResult {
-            ok: drives.iter().filter_map(|&item| match &item.len() {
-                0 => None,
-                _ => {
-                    let name = item.to_string();
-                    let drive_type = get_drive_type(&name);
-                    Some(RootItem { 
-                        name: name.clone(),
-                        description: get_drive_description(&name),
-                        size: get_volume_size(&name) as usize,
-                        mount_point: "".to_string(),
-                        drive_type: "".to_string(),
-                        is_mounted: if drive_type == DriveType::HARDDRIVE { true} else { is_mounted(&name) }
-                    }) 
-                }
-            }).collect::<Vec<RootItem>>()
-        }
+        }).collect::<Vec<RootItem>>()
     }
 }
 
