f155216a6a800f013324de673d1d450e17a4a018
uriegel <uriegel@hotmail.de>
2024-11-14 20:50:54 +0100

    getfiles

--- src/remote.rs
+++ src/remote.rs
@@ -15,6 +15,6 @@ pub fn get_remote_files(input: GetRemoteFiles) -> Result<(), RequestError> {
     let (_, path) = input.path.split_at(7);
     let sep = path.find("/").unwrap_or(path.len());
     let (ip, path) = path.split_at(sep);
-    web_get(ip, format!("getfiles{}", path))?;
+    web_get(ip, format!("/getfiles{}", path))?;
     Ok(())
 }
--- src/webrequest.rs
+++ src/webrequest.rs
@@ -1,8 +1,34 @@
-use std::net::TcpStream;
+use std::{io::{BufRead, BufReader, BufWriter, Read, Write}, net::TcpStream};
 
 use crate::request_error::RequestError;
 
 pub fn web_get(ip: &str, url: String) -> Result<(), RequestError> {
-    let mut stream = TcpStream::connect(format!("{}:8080", ip))?; 
+    let stream = TcpStream::connect(format!("{}:8080", ip))?; 
+    let mut buf_writer = BufWriter::new(&stream);
+    let mut buf_reader = BufReader::new(&stream);
+    let payload = format!("GET {url} HTTP/1.1\r\n\r\n");
+    buf_writer.write_all(payload.as_bytes())?;
+    buf_writer.flush()?;
+    let mut result_string = String::new();
+
+    let mut headers: Vec<String> = Vec::new();
+    loop {
+        let mut str = String::new();
+        buf_reader.read_line(&mut str)?;
+        str = str.trim().to_string();
+        if str.len() == 0 {
+            break;
+        }
+        headers.push(str);
+    }
+
+    // TODO Read Status Code
+    // TODO read content_length
+    // TODO payload
+
+    if headers.len() == 0  { 
+        return Ok(())
+    }
+
     Ok(())
 }
