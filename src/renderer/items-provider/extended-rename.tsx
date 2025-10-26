11c62566e3f4f5ab557577e3ab5ddd565194f871
Uwe Riegel <uriegel@hotmail.de>
2025-10-26 07:44:05 +0100

    Extended rename parts

--- src/renderer/items-provider/extended-rename.tsx
+++ src/renderer/items-provider/extended-rename.tsx
@@ -1,19 +1,20 @@
 import { DialogHandle } from "web-dialog-react"
 import { IItemsProvider } from "./base-provider"
+import ExtendedRename, { ExtendedRenameProps } from "../components/dialogs/ExtendedRename"
 
 export const showExtendedRename = async (currentItemsProvider: IItemsProvider|undefined, dialog: DialogHandle) => {
-    const res = await dialog.show({
-         text: "Erweitertes Umbenennen",
-    //     extension: ExtendedRenamePart,
-    //     extensionProps: {
-    //         prefix: localStorage.getItem("extendedRenamePrefix") ?? "Bild",
-    //         digits: localStorage.getItem("extendedRenameDigits")?.parseInt() ?? 3,
-    //         startNumber: localStorage.getItem("extendedRenameStartNumber")?.parseInt() ?? 1
-    //     } as ExtendedRenameProps,
-         btnOk: true,
-         btnCancel: true,
-         defBtnOk: true
-    })
+     const res = await dialog.show({
+          text: "Erweitertes Umbenennen",
+          extension: ExtendedRename,
+          extensionProps: {
+               prefix: localStorage.getItem("extendedRenamePrefix") ?? "Bild",
+               digits: localStorage.getItem("extendedRenameDigits")?.parseInt() ?? 3,
+               startNumber: localStorage.getItem("extendedRenameStartNumber")?.parseInt() ?? 1
+          } as ExtendedRenameProps,
+          btnOk: true,
+          btnCancel: true,
+          defBtnOk: true
+     })
     // return (res.result == ResultType.Ok && !(currentController instanceof ExtendedRename))
     //     ? new ExtendedRename()
     //     : (res.result != ResultType.Ok && (currentController instanceof ExtendedRename))
