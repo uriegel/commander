9321432ddda927a16356c165ce5dba3d0713430f
Uwe Riegel <uriegel@hotmail.de>
2026-04-17 23:52:48 +0200

    WebWindowNetCore

--- Commander/Main.cs
+++ Commander/Main.cs
@@ -1,1 +1,20 @@
-Console.WriteLine("Da bin ich ja");
+using System.Drawing;
+using WebWindowNetCore;
+
+WebView
+    .Create()
+    .AppId("de.uriegel.commander")
+    .Title("Commander")
+    .InitialBounds(600, 800)
+    .SaveBounds()
+    .DevTools()
+    .BackgroundColor(Color.Transparent)
+    //.DefaultContextMenuDisabled()
+#if Windows
+    .ResourceIcon("icon")
+#endif
+    .DebugUrl("http://localhost:5173/")
+    .Url("https://github.com")
+    .CanClose(() => true)
+    .Run();
+
