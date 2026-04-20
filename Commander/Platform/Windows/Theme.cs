5b6dce5dc4a750fb5f67e5c696c1224a4dcf637d
Uwe Riegel <uriegel@hotmail.de>
2026-04-20 07:40:16 +0200

    AccentColor Windows

--- Commander/Platform/Windows/Theme.cs
+++ Commander/Platform/Windows/Theme.cs
@@ -2,7 +2,8 @@
 
 static class Theme
 {
-    public static string GetAccentColor() => "lightblue";
+    static bool darkMode = false;
+    public static string GetAccentColor() => darkMode ? "#0073e5" : "#0070de";
 
     public static void StartChangeDetecting() { }
 }
