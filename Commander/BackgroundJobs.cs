7d1b68b5489abd49c063227ea0540a5840f88f01
Uwe Riegel <uriegel@hotmail.de>
2026-04-28 13:57:41 +0200

    Job processing

--- Commander/BackgroundJobs.cs
+++ Commander/BackgroundJobs.cs
@@ -4,13 +4,54 @@ static class BackgroundJobs
 {
     public static bool IsIdle() => !jobs.Reader.TryPeek(out var _);
 
-    public static void Test()
+    static BackgroundJobs()
     {
-        jobs.Writer.TryWrite("");
+        jobs = Channel.CreateUnbounded<JobBase>();
+        jobProcessorTask = Task.Run(RunProcessing);
     }
 
-    static BackgroundJobs()
-        => jobs = Channel.CreateUnbounded<string>();
+    static async Task RunProcessing()
+    {
+        try
+        {
+            await foreach (var n in jobs.Reader.ReadAllAsync())
+                await Process(n);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine($"Exception in background processing: {e}");
+        }
+    }
+    
+    static async Task Process(JobBase job)
+    {
+        try
+        {
+            // job.Cancellation?.ThrowIfCancellationRequested();
 
-    static readonly Channel<string> jobs;
+            // var result = await (job switch
+            // {
+            //     MonitorJob mj => proxy.AddPartnerInternalAsync(mj.SessionID, mj.Name),
+            //     MonitorByExtensionJob mej => proxy.AddPartnerByExtensionInternalAsync(mej.SessionID, mej.Extension),
+            //     MonitorByDeviceJob mdj => proxy.AddPublicMonitorDeviceInternalAsync(mdj.SessionID, mdj.Device, mdj.GeneratedGuid),
+            //     _ => throw new InvalidOperationException()
+            // });
+
+            // job.Completion.TrySetResult(result);
+        }
+        catch (OperationCanceledException)
+        {
+//            job.Completion.TrySetCanceled();
+        }
+        catch (Exception e)
+        {
+//            job.Completion.TrySetException(e);
+        }
+    }
+
+
+    static readonly Channel<JobBase> jobs;
+    static readonly Task jobProcessorTask;
 }
+
+record JobBase();
