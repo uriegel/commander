64f19409bb5211eff2cd8f51499abc49c13624b9
Uwe Riegel <uriegel@hotmail.de>
2026-06-06 07:11:00 +0200

    Gtk4DotNet

--- Commander/Main.cs
+++ Commander/Main.cs
@@ -6,8 +6,6 @@ WebView.Run();
 Icon.StopProcessing();    
 server.Stop();
 
-// TODO Eject and Unmount in GTK4 
-// TODO new GTK4 to WebWindowNetCore
 // TODO GetFiles has to return mount info => save mount info as lastPath
 // TODO Favorites with display names and mount info
 // TODO Unmount and Eject removable devices in root view 
