81299bf2eeac3522ed983296b4ad845792dc638a
Uwe Riegel <uriegel@hotmail.de>
2026-06-17 15:18:29 +0200

    Linux version

--- Commander/Requests.cs
+++ Commander/Requests.cs
@@ -310,6 +310,12 @@ static class Requests
         => WebView.Window.RunJavascript($"onEvent('{JsonSerializer.Serialize(evt, Json.Defaults)}')");
 }
 
-
+static class PayloadExtensions
+{
+    public static bool IsSvg(this byte[] payload)
+        => payload.Length > 4
+            && (payload[0] == 60 && payload[1] == 115 && payload[2] == 118 && payload[3] == 103
+            || payload[0] == 60 && payload[1] == 63 && payload[2] == 120 && payload[3] == 109);    
+}
 
 
